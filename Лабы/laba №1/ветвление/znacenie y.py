b1462d5725006e852c1dd55247caefc191774000
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-06-06 13:22:04 +0300

    Delete Лабы directory

--- Лабы/laba №1/ветвление/znacenie y.py
+++
@@ -1,8 +0,0 @@
-from math import sin, cos, log
-z = float(input("Введите значение z: "))
-if z >= 0:
-    x = 2*z+1
-else:
-    x = log(z**2-z)
-y = (sin(x)**2)+(cos(x**3)**5)+(log(x**(2/5)))
-print("y=","%.3f"%y)
--- Лабы/laba №1/циклические/chisla bez povtorov.py
+++
@@ -1,7 +0,0 @@
-print("Все четырёхзначные числа в которых нет одинаковых цифр")
-for a in range(1,10):
-    for b in range(0,10):
-        for c in range(0,10):
-            for d in range(0,10):
-                if (a != b) and if (a != c) and if (a != d) and if (b != c) and if (b != d) and if (c != d):
-                    print(a,b,c,d)
